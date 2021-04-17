8f76c4d805ea9eefda0c517a819b2ff01a93cc6e
Filip Sobalski <pinkeen@gmail.com>
2021-04-17 21:01:42 +0200

    Allow composer v2 API
    Change the constraint to allow composer v2 API and add missing PluginInterface methods.
    
    It's not possible to run tests with this version, however, it will probably work correctly
    because the plugin does not rely on any API that has been deprecated, modified or changed
    its behaviour in composer v2.

--- src/Plugin.php
+++ src/Plugin.php
@@ -38,6 +38,21 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     /**
      * {@inheritdoc}
      */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
     public static function getSubscribedEvents()
     {
         return [
